c92f75d613b548fe53347f4199e1445862f9ac2a
Jeremy Liss <jeremyliss@gmail.com>
2017-08-25 15:25:20 +1200

    add moveInvaders left test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var test = require('tape')
 var moveInvaders = require('../lib/moveInvaders')
 
-test('moveInvaders updates state to show movement of invaders to right', function (t) {
+test('moveInvaders updates state and invaders move right', function (t) {
 
   var testState = {
     right: true,
@@ -19,5 +19,26 @@ test('moveInvaders updates state to show movement of invaders to right', functio
   t.equal(testState.invadersPosition.marginLeft, expected.marginLeft, 'marginLeft increased by 50')
   t.equal(testState.invaders[0][0].activeImage, expected.activeImage, 'activeImage is image1')
   t.false(testState.right, 'right should be false')
+  t.end()
+})
+
+test('moveInvaders updates state and invaders move left', function (t) {
+
+  var testState = {
+    right: false,
+    invadersPosition: {marginLeft: -150},
+    invaders: [[{activeImage: '', image1: 'image1'}]]
+  }
+
+  var expected = {
+    marginLeft: -200,
+    activeImage: 'image1'
+  }
+
+  moveInvaders.moveInvaders(testState)
+
+  t.equal(testState.invadersPosition.marginLeft, expected.marginLeft, 'marginLeft decreased by 50')
+  t.equal(testState.invaders[0][0].activeImage, expected.activeImage, 'activeImage is image1')
+  t.true(testState.right, 'right should be true')
   t.end()
 })
