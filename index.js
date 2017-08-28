d0a36368bc123d4875e5c5e06a5fc1e3cb386146
Jeremy Liss <jeremyliss@gmail.com>
2017-08-29 11:54:27 +1200

    randomize bonus ship, fix position bug

--- index.js
+++ index.js
@@ -36,15 +36,24 @@ const moveBonus = () => {
     state.bonus.style.left -= 50
     if (state.bonus.style.left === 100){
       state.bonus.visible = false
-      state.bonus.style.left = 1000
     }
   }, 1000/10)
 }
 
-setInterval( () => {
+const triggerMoveBonus = () => {
   state.bonus.visible = true
+  state.bonus.style.left = 1000
   moveBonus()
-}, 5000)
+}
+
+(function loop() {
+    let times = [10000, 15000, 20000]
+    let time = times[Math.floor(Math.random() * times.length)]
+    setTimeout(function() {
+      triggerMoveBonus()
+      loop()
+    }, time)
+}())
 
 const target = document.getElementById('root')
 
